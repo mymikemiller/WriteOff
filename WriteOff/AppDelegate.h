6f0a949f33ce3d26ae93aa34e5b63270b54783e7
Mike Miller <mike@mikes-mac-mini.lan>
2012-04-14 11:58:53 -0700

    Initial Commit

---
+++ WriteOff/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WriteOff
+//
+//  Created by Mike Miller on 4/14/12.
+//  Copyright (c) 2012 Mike Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WriteOff/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WriteOff
+//
+//  Created by Mike Miller on 4/14/12.
+//  Copyright (c) 2012 Mike Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
